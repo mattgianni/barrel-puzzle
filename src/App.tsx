6c27bb5d687ff1429e5890ade813a6bf3c1ff8bb
Matthew Gianni <matt@gianni.org>
2024-07-18 18:20:25 -0700

    Move to tailwindcss Fixes #9

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
-import Game from './components/Game'
+import Game from "./components/Game";
 
 function App() {
-
-  return (
-    <>
-      <Game />
-    </>
-  )
+    return (
+        <>
+            <Game />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Peg.tsx
+++ src/components/Peg.tsx
@@ -1,28 +1,34 @@
 // import { MouseEventHandler } from 'react'
 
-import { GameStateManager } from '../lib/GameState'
-import './Peg.css'
+import { GameStateManager } from "../lib/GameState";
+
 interface PegProps {
-    index: number,
-    game: GameStateManager
+    index: number;
+    game: GameStateManager;
 }
 
 const Peg = (props: PegProps) => {
-    const index = props.index
-    const game = props.game
+    const index = props.index;
+    const game = props.game;
 
-    const selected = index == game.state.selected
-    const selectable = game.isSelectable(index)
-    const filled = game.state.board[index]
-
-    const className = `peg${selected ? " selectedpeg" : ""}${selectable ? " selectablepeg" : ""}${filled ? " filledpeg" : ""}`
-    // const status = game.state.board[index]
+    const selected = index == game.state.selected;
+    const selectable = game.isSelectable(index);
+    const filled = game.state.board[index];
 
     return (
         <>
-            <div className={className} onClick={() => game.handleClick(index)}>{index}</div>
+            <div
+                className={`h-[23px] w-[23px] border-[1px] border-black border-solid rounded-[23px] m-[3px] text-center text-[0.6em] leading-[23px] align-middle p-0${
+                    selected ? " bg-red-500 text-white font-bold" : ""
+                }${!selected && selectable ? " hover:cursor-pointer" : ""}${
+                    !selected && filled ? " bg-blue-800 text-white" : ""
+                }`}
+                onClick={() => game.handleClick(index)}
+            >
+                {index}
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Peg
+export default Peg;
